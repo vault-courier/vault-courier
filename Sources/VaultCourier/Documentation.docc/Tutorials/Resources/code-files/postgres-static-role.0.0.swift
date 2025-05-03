2b437a3a04b721d563c66469ef7fe7caf37aa25a
Javier Cuesta <jav.cuesta@gmail.com>
2025-05-03 17:08:07 +0200

    Add source code for static role section

---
+++ Sources/VaultCourier/Documentation.docc/Tutorials/Resources/code-files/postgres-static-role.0.0.swift
@@ -0,0 +1,27 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
+import ArgumentParser
+import OpenAPIAsyncHTTPClient
+import VaultCourier
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#else
+import struct Foundation.URL
+#endif
+
+@main
+struct StaticRoleCredentials: ParsableCommand {
+    @Option(name: .shortAndLong)
+    var enginePath: String = "database"
+
+    @Option(name: .shortAndLong)
+    var connectionName: String = "pg_connection"
+
+    mutating func run() throws {
+        print("Hello, world!")
+    }
+}
---
+++ Sources/VaultCourier/Documentation.docc/Tutorials/Resources/code-files/postgres-static-role.0.2.swift
@@ -0,0 +1,52 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
+import ArgumentParser
+import OpenAPIAsyncHTTPClient
+import VaultCourier
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#else
+import struct Foundation.URL
+#endif
+
+@main
+struct StaticRoleCredentials: AsyncParsableCommand {
+    @Option(name: .shortAndLong)
+    var enginePath: String = "database"
+
+    @Option(name: .shortAndLong)
+    var connectionName: String = "pg_connection"
+
+    var roleName: String = "static_role"
+
+    mutating func run() async throws {
+        let vaultClient = try Self.makeVaultClient()
+        try await vaultClient.authenticate()
+
+        try await vaultClient.create(staticRole: .init(vaultRoleName: roleName,
+                                                       databaseUsername: "static_role_username",
+                                                       databaseConnectionName: connectionName,
+                                                       rotationPeriod: "28d"),
+                                     enginePath: enginePath)
+    }
+
+    static func makeVaultClient() throws -> VaultClient {
+        let vaultURL = URL(string: "http://127.0.0.1:8200/v1")!
+        let config = VaultClient.Configuration(apiURL: vaultURL)
+
+        let client = Client(
+            serverURL: vaultURL,
+            transport: AsyncHTTPClientTransport()
+        )
+
+        return VaultClient(
+            configuration: config,
+            client: client,
+            authentication: .token("education")
+        )
+    }
+}
