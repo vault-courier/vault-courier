271ef80a5b6829f586bb695c654d18bcca235cd6
Javier Cuesta <jav.cuesta@gmail.com>
2025-05-14 21:57:49 +0200

    Start payload tests and add duration conversion

---
+++ Tests/VaultCourierTests/Pkl+VaultCourierPayloads.swift
@@ -0,0 +1,60 @@
+//===----------------------------------------------------------------------===//
+//  Copyright (c) 2025 Javier Cuesta
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//===----------------------------------------------------------------------===//
+
+import Testing
+
+import OpenAPIRuntime
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#else
+import Foundation
+#endif
+import PklSwift
+
+@testable import VaultCourier
+
+extension IntegrationTests.Pkl {
+    struct Payloads {
+        let localApiURL = try! URL(validatingOpenAPIServerURL: "http://127.0.0.1:8200/v1")
+        var configuration: VaultClient.Configuration { .init(apiURL: localApiURL) }
+
+        @Test
+        func create_database_static_role_with_pkl_file() async throws {
+            let url = pklFixtureUrl(for: "test_static_role.pkl")
+
+            await #expect(throws: Never.self) {
+                let config = try await PostgresStaticRole.loadFrom(source: .url(url))
+
+                guard CreateDatabaseStaticRole(config) != nil else {
+                    Issue.record("Failed to init CreateDatabaseStaticRole from pkl-generated payload")
+                    return
+                }
+            }
+        }
+
+        @Test
+        func vault_duration_style_for_seconds() async throws {
+            let duration = Swift.Duration(secondsComponent: 5400, attosecondsComponent: 0)
+            #expect(duration.formatted(.vaultSeconds) == "5400s")
+        }
+
+        @Test
+        func vault_duration_style_for_hours() async throws {
+            let duration = Swift.Duration(secondsComponent: 5400, attosecondsComponent: 0)
+            #expect(duration.formatted(.vaultHours) == "1.5h")
+        }
+    }
+}
