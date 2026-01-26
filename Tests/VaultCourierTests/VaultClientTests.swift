42739339b741d4fc7bdca990302ac6adc0a337b2
Javier Cuesta <jav.cuesta@gmail.com>
2026-01-26 22:54:34 +0100

    add token lookup mock request

--- Tests/VaultCourierTests/VaultClientTests.swift
+++ Tests/VaultCourierTests/VaultClientTests.swift
@@ -151,6 +151,39 @@ struct VaultClientTests {
         let expectedSecretID = "ed8313c8-852a-0098-0d3f-67c5771cca6e"
         let mockClient = MockVaultClientTransport { req, _, _, _ in
             switch req.normalizedPath {
+                case "/auth/token/lookup-self":
+                    return (.init(status: .ok), .init("""
+                        {
+                          "request_id": "f1974b8f-266e-2dd6-3ed3-13d330d4589e",
+                          "lease_id": "",
+                          "renewable": false,
+                          "lease_duration": 0,
+                          "data": {
+                            "accessor": "ufPPb3VC6rTBCIOqtZydD0bu",
+                            "creation_time": 1769460540,
+                            "creation_ttl": 0,
+                            "display_name": "token",
+                            "entity_id": "",
+                            "expire_time": null,
+                            "explicit_max_ttl": 0,
+                            "id": "integration_token",
+                            "issue_time": "2026-01-26T20:49:00.428750095Z",
+                            "meta": null,
+                            "num_uses": 0,
+                            "orphan": true,
+                            "path": "auth/token/create",
+                            "policies": [
+                              "root"
+                            ],
+                            "renewable": false,
+                            "ttl": 0,
+                            "type": "service"
+                          },
+                          "wrap_info": null,
+                          "warnings": null,
+                          "auth": null
+                        }
+                        """))
                     case "/auth/\(mountPath)/role/\(appRoleName)/secret-id":
                     return (.init(status: .ok), .init("""
                         {
